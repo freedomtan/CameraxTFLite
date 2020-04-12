b490e173d2e4cb17e52ea69f65a8519a0f58c1a7
Koan-Sin Tan <koansin.tan@gmail.com>
2020-04-12 11:55:07 +0800

    use TFLite Mobilenet V1 to label input bitmap

--- app/src/main/java/com/mediatek/cameraxtflite/MainActivity.kt
+++ app/src/main/java/com/mediatek/cameraxtflite/MainActivity.kt
@@ -2,19 +2,22 @@ package com.mediatek.cameraxtflite
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Matrix
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.*
+import android.media.Image
 import android.os.Bundle
 import android.util.Size
 import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import org.tensorflow.lite.classify.MyClassifierModel
+import org.tensorflow.lite.support.model.Model
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.Executors
 
 // This is an arbitrary number we are using to keep track of the permission
@@ -24,6 +27,29 @@ private const val REQUEST_CODE_PERMISSIONS = 10
 
 // This is an array of all the permission specified in the manifest.
 private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+fun Image.toBitmap(): Bitmap {
+    val yBuffer = planes[0].buffer // Y
+    val uBuffer = planes[1].buffer // U
+    val vBuffer = planes[2].buffer // V
+
+    val ySize = yBuffer.remaining()
+    val uSize = uBuffer.remaining()
+    val vSize = vBuffer.remaining()
+
+    val nv21 = ByteArray(ySize + uSize + vSize)
+
+    // U and V are swapped
+    yBuffer.get(nv21, 0, ySize)
+    vBuffer.get(nv21, ySize, vSize)
+    uBuffer.get(nv21, ySize + vSize, uSize)
+
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+    val out = ByteArrayOutputStream()
+    yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+    val imageBytes = out.toByteArray()
+    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,19 +64,25 @@ class MainActivity : AppCompatActivity() {
             cameraView.post { startCamera() }
         } else {
             ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
 
         // Every time the provided texture view changes, recompute layout
         cameraView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
             updateTransform()
         }
+
+        latencyText = findViewById(R.id.latency)
+        top1Text = findViewById(R.id.top1)
     }
 
     // Add this after onCreate
 
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var cameraView: TextureView
+    private lateinit var latencyText: TextView
+    private lateinit var top1Text: TextView
 
     private fun startCamera() {
         // TODO: Implement CameraX operations
@@ -75,11 +107,25 @@ class MainActivity : AppCompatActivity() {
             updateTransform()
         }
 
+        // Setup image analysis pipeline that do Mobilenet V1 classification
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE
+            )
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, TfLiteAnalyzer(this@MainActivity))
+        }
+
         // Bind use cases to lifecycle
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, analyzerUseCase)
     }
 
     private fun updateTransform() {
@@ -90,7 +136,7 @@ class MainActivity : AppCompatActivity() {
         val centerY = cameraView.height / 2f
 
         // Correct preview output to account for display rotation
-        val rotationDegrees = when(cameraView.display.rotation) {
+        val rotationDegrees = when (cameraView.display.rotation) {
             Surface.ROTATION_0 -> 0
             Surface.ROTATION_90 -> 90
             Surface.ROTATION_180 -> 180
@@ -108,14 +154,17 @@ class MainActivity : AppCompatActivity() {
      * been granted? If yes, start Camera. Otherwise display a toast
      */
     override fun onRequestPermissionsResult(
-            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 cameraView.post { startCamera() }
             } else {
-                Toast.makeText(this,
-                        "Permissions not granted by the user.",
-                        Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
                 finish()
             }
         }
@@ -126,6 +175,41 @@ class MainActivity : AppCompatActivity() {
      */
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-                baseContext, it) == PackageManager.PERMISSION_GRANTED
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private class TfLiteAnalyzer(outer: MainActivity) : ImageAnalysis.Analyzer {
+        private val mainActivity = outer
+
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+            val bitmap = image?.getImage()?.toBitmap()
+            val myImageClassifier =
+                MyClassifierModel(mainActivity.getApplicationContext(), Model.Device.NNAPI, 4)
+
+            var inputs = myImageClassifier.createInputs()
+            inputs.loadImage(bitmap)
+
+            val startTimestamp = System.nanoTime()
+            var outputs = myImageClassifier.run(inputs)
+            val stopTimestamp = System.nanoTime()
+
+            var labeledProbability = outputs.getProbability()
+
+            var maxEntry: Map.Entry<String, Float>? = null
+            for (entry in labeledProbability.entries) {
+                if (maxEntry == null || entry.value.compareTo(maxEntry.value) > 0) {
+                    maxEntry = entry;
+                }
+            }
+
+            mainActivity.runOnUiThread(Runnable {
+                mainActivity.top1Text.text = "top-1: " + maxEntry.toString()
+            })
+            mainActivity.runOnUiThread(Runnable {
+                mainActivity.latencyText.text =
+                    "latency: " + (stopTimestamp - startTimestamp) / 1000000.0 + " ms"
+            })
+        }
     }
 }
